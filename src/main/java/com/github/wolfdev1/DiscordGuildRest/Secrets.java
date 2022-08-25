32ac2a0f90b5dd6ddeb30d8f6852622df9b2eb50
WolfDevMC <91499295+wolfdev1@users.noreply.github.com>
2022-08-25 22:17:51 +0200

    Update Secrets.java

--- src/main/java/com/github/wolfdev1/DiscordGuildRest/Secrets.java
+++ src/main/java/com/github/wolfdev1/DiscordGuildRest/Secrets.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.entities.Member;
 
 public class Secrets {
 
-    public static final String BOT_TOKEN = "ODc0NzMxMjEyNTcxMjc5NDAx.GN9yoV.gebJv_gMm-5opGhHNwo6R-o_Z0V_9sarI1k2OM";
+    public static final String BOT_TOKEN = "HERE YOUR TOKEN";
    
   
 }
